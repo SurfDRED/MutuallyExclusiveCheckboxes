bc2df12a8bdf7bce3f43e5a10aee58cb6632f69f
Dmitii Iachmentsev <d.yachmentsev@gmail.com>
2020-05-24 20:24:11 +0200

    initial

---
+++ app/src/main/java/com/example/mutuallyexclusivecheckboxes/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.mutuallyexclusivecheckboxes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputMoney;
+    private EditText mInputInfo;
+    private Button mBtnOk;
+    private CheckBox mBankCardChkBx;
+    private CheckBox mMobilePhoneChkBx;
+    private CheckBox mCashAddressChkBx;
+    private String miniInfo;
+    private boolean statusPaid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void resetCheckBoxes(){
+        mBankCardChkBx.setChecked(false);
+        mMobilePhoneChkBx.setChecked(false);
+        mCashAddressChkBx.setChecked(false);
+    }
+
+    private void initViews() {
+        mInputMoney = findViewById(R.id.inputMoney);
+        mInputInfo = findViewById(R.id.inputInfo);
+        mBtnOk = findViewById(R.id.btnOK);
+        mBankCardChkBx = findViewById(R.id.bankCardChkBx);
+        mMobilePhoneChkBx = findViewById(R.id.mobilePhoneChkBx);
+        mCashAddressChkBx = findViewById(R.id.cashAddressChkBx);
+        mBankCardChkBx.setOnCheckedChangeListener(checkedChangeListener);
+        mMobilePhoneChkBx.setOnCheckedChangeListener(checkedChangeListener);
+        mCashAddressChkBx.setOnCheckedChangeListener(checkedChangeListener);
+
+        mBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userMoney = mInputMoney.getText().toString();
+                String userInfo = mInputInfo.getText().toString();
+
+                if (userMoney.isEmpty() || userInfo.isEmpty() || statusPaid == Boolean.FALSE) {
+                    Toast.makeText(MainActivity.this, getString(R.string.notEnoughData), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, miniInfo + " " + userInfo, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if (b) {
+                switch (compoundButton.getId()) {
+                    case R.id.bankCardChkBx:
+                        resetCheckBoxes();
+                        mBankCardChkBx.setChecked(true);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        mInputInfo.setHint("Только цифры");
+                        miniInfo = "Оплата с карты";
+                        statusPaid = true;
+                        break;
+                    case R.id.mobilePhoneChkBx:
+                        resetCheckBoxes();
+                        mMobilePhoneChkBx.setChecked(true);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_PHONE);
+                        mInputInfo.setHint("Только номер телефона");
+                        miniInfo = "Оплата с телефона";
+                        statusPaid = true;
+                        break;
+                    case R.id.cashAddressChkBx:
+                        resetCheckBoxes();
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_TEXT);
+                        mCashAddressChkBx.setChecked(true);
+                        mInputInfo.setHint("Любой текст");
+                        miniInfo = "Оплата в банке";
+                        statusPaid = true;
+                        break;
+                    default:
+                }
+            } else   {
+                statusPaid = false;
+            }
+        }
+    };
+}
